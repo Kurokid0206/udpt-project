9bdf9a22188bcce16a5ad601651f7c0213444e28
Mean-Sun <tranminhsonyi@gmail.com>
2023-07-06 21:12:22 +0700

    feat: setup redux 2

--- label-stick-fe/src/App.tsx
+++ label-stick-fe/src/App.tsx
@@ -3,16 +3,20 @@ import "./App.css";
 import { QueryClient, QueryClientProvider } from "react-query";
 import theme from "@utils/theme";
 import NavigationRouter from "@routers/router";
+import store from "@redux/store";
+import { Provider } from "react-redux";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        <NavigationRouter />
-      </ThemeProvider>
-    </QueryClientProvider>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <NavigationRouter />
+        </ThemeProvider>
+      </QueryClientProvider>
+    </Provider>
   );
 }
 
